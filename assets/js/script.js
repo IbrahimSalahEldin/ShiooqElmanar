dd54bef0482da9867d21d6aff2cbc466d31dcdd0
IbrahimSalahEldin <ibrahimsalah824@gmail.com>
2024-07-28 06:31:12 +0300

    up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
 // JavaScript (script.js)
 
 // WhatsApp Button Click Event
-document.getElementById('whatsapp-link').addEventListener('click', function() {
-    // Replace '123456789' with your full international WhatsApp number
-    var phoneNumber = "9660531020893"; // Include your country code (e.g., +1 for the USA)
-    
-    // Open WhatsApp with the given number
-    window.open('https://wa.me/c/' + phoneNumber, '_blank');
+document.getElementById("whatsapp-link").addEventListener("click", function () {
+  // Replace '123456789' with your full international WhatsApp number
+  var phoneNumber = "9660531020893"; // Include your country code (e.g., +1 for the USA)
+
+  // Open WhatsApp with the given number
+  window.open("https://wa.me/" + phoneNumber, "_blank");
 });
+
 // Call Button Click Event
 document.getElementById('call-link').addEventListener('click', function() {
     // Prompt the user to confirm if they want to make a call
